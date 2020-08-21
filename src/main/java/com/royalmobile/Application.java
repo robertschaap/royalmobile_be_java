b8258a5df48ff698fafaeeda1d2d3ce758c32d8a
robertschaap <robert.schaap@mac.com>
2020-08-21 21:04:02 +0200

    feat: setup spring application

--- src/main/java/com/royalmobile/Application.java
+++ src/main/java/com/royalmobile/Application.java
@@ -1,7 +1,12 @@
 package com.royalmobile;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    SpringApplication.run(Application.class, args);
+    System.out.println("Running on http://localhost:8008");
   }
 }
---
+++ src/main/java/com/royalmobile/Controller.java
@@ -0,0 +1,12 @@
+package com.royalmobile;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  @GetMapping("/")
+  public String hello() {
+    return "Hello World";
+  }
+}
