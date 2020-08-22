11dbe5ddcccd11e630d410400910dd4551039ce6
robertschaap <robert.schaap@mac.com>
2020-08-22 20:23:35 +0200

    style(Application): sort imports

--- src/main/java/com/royalmobile/Application.java
+++ src/main/java/com/royalmobile/Application.java
@@ -1,7 +1,7 @@
 package com.royalmobile;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class Application {
