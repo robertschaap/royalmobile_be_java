71529df20ad026a70b4e256522e78e058fc2262b
robertschaap <robert.schaap@mac.com>
2020-08-24 18:37:53 +0200

    feat(SubscriptionsModel): setup basic model

---
+++ src/main/java/com/royalmobile/models/SubscriptionsModel.java
@@ -0,0 +1,10 @@
+package com.royalmobile.models;
+
+public class SubscriptionsModel {
+
+  private Subscription subscriptions = new Subscription("20gb", "1year");
+
+  public Subscription getSubscriptions() {
+    return this.subscriptions;
+  }
+}
