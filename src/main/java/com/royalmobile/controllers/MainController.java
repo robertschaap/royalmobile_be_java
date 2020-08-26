fcc7d5c53be142c241874fed331e1a6991c2a253
robertschaap <robert.schaap@mac.com>
2020-08-26 20:32:53 +0200

    feat(MainController): add response type

--- src/main/java/com/royalmobile/controllers/MainController.java
+++ src/main/java/com/royalmobile/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-  @GetMapping("/")
+  @GetMapping(value = "/", produces = { "application/json" })
   public String getIndex() {
     return ApiResponse.success("Hello World");
   }
