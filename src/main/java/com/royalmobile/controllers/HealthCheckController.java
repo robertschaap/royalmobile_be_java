2ab7ef9d09371eb960cdfce16faa2358da4ea116
robertschaap <robert.schaap@mac.com>
2020-08-26 20:32:40 +0200

    feat(HealthCheckController): add response type

--- src/main/java/com/royalmobile/controllers/HealthCheckController.java
+++ src/main/java/com/royalmobile/controllers/HealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-  @GetMapping("/healthcheck")
+  @GetMapping(value = "/healthcheck", produces = { "application/json" })
   public String getHealthCheck() {
     return ApiResponse.success("Application is up and running");
   }
