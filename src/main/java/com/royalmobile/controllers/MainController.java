0d5330969435c68d2e14252d605742e984182c28
robertschaap <robert.schaap@mac.com>
2020-08-26 20:00:44 +0200

    feat(MainController): update return type on get route

--- src/main/java/com/royalmobile/controllers/MainController.java
+++ src/main/java/com/royalmobile/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
   @GetMapping("/")
-  public ApiResponse getIndex() {
+  public String getIndex() {
     return ApiResponse.success("Hello World");
   }
 }
