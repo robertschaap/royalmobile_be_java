f9c12c6375df40f3a7b8811f592e31a56bc41168
robertschaap <robert.schaap@mac.com>
2020-08-27 20:24:33 +0200

    feat(ProductsModel): programatically make products

--- src/main/java/com/royalmobile/models/ProductsModel.java
+++ src/main/java/com/royalmobile/models/ProductsModel.java
@@ -1,11 +1,36 @@
 package com.royalmobile.models;
 
-public class ProductsModel {
-  private Product[] products = {
-    new Product(0, "Apple", "iPhone X1", "apple-iphonex1")
-  };
+import java.util.ArrayList;
+import java.util.List;
 
-  public Product[] getProducts() {
+public class ProductsModel {
+
+  private Integer nextProductId = 0;
+
+  private List<Product> products = new ArrayList<Product>();
+
+  public ProductsModel() {
+    this.initProductsModel();
+  }
+
+  public List<Product> getProducts() {
     return this.products;
   }
+
+  public void initProductsModel() {
+
+    // TODO: one loop for now for developer sanity while ProductVariant is being built
+    for (int i = 0; i < 1; i++) {
+
+      String model = "iPhone X" + Integer.toString(i + 1);
+      String modelId = "apple-iphonex" + Integer.toString(i + 1);
+      Product product = new Product(this.nextProductId, "Apple", model, modelId);
+
+      this.products.add(product);
+      this.nextProductId++;
+    }
+  }
 }
+
+// 16 - 32 - 64 - 128
+// lime, purple, green, pink
