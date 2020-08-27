47da25f36e39bc5eb7cf80ae17988a008cdce25b
robertschaap <robert.schaap@mac.com>
2020-08-27 19:39:27 +0200

    feat(ProductsController): setup basic controller

---
+++ src/main/java/com/royalmobile/controllers/ProductsController.java
@@ -0,0 +1,19 @@
+package com.royalmobile.controllers;
+
+import com.royalmobile.ApiResponse;
+import com.royalmobile.models.ProductsModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+
+  private ProductsModel productsModel = new ProductsModel();
+
+  @GetMapping(value = "/api/products", produces = { "application/json" })
+  public String getProducts() {
+    return ApiResponse.success(
+      this.productsModel.getProducts()
+    );
+  }
+}
