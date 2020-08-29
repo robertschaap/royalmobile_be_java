56b11f878543194736d16d1a43e7f9c74d7c0e30
robertschaap <robert.schaap@mac.com>
2020-08-29 09:39:13 +0200

    feat(ProductController): add getProduct method

--- src/main/java/com/royalmobile/controllers/ProductController.java
+++ src/main/java/com/royalmobile/controllers/ProductController.java
@@ -1,13 +1,22 @@
 package com.royalmobile.controllers;
 
+import com.royalmobile.ApiResponse;
+import com.royalmobile.models.ProductsModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
-  @GetMapping(value = "/api/product", produces = { "application/json" })
-  public String getProduct() {
-    return "Get Product";
+  private ProductsModel productsModel = new ProductsModel();
+
+  @GetMapping(value = "/api/product/{modelId}", produces = { "application/json" })
+  public String getProduct(@PathVariable String modelId) {
+    try {
+      return ApiResponse.success(this.productsModel.getProductByModelId(modelId));
+    } catch (Exception e) {
+      return ApiResponse.error("Not found");
+    }
   }
 }
