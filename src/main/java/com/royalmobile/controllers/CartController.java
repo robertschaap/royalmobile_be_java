3b6354ca345df79ab86a3aeecfecac404b74ba2a
robertschaap <robert.schaap@mac.com>
2020-08-29 10:17:23 +0200

    feat(CartController): setup basic controller

---
+++ src/main/java/com/royalmobile/controllers/CartController.java
@@ -0,0 +1,18 @@
+package com.royalmobile.controllers;
+
+import com.royalmobile.ApiResponse;
+import com.royalmobile.models.CartModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartController {
+
+  private CartModel cartModel = new CartModel();
+
+  @GetMapping(value = "/api/cart/{cartId}", produces = { "application/json" })
+  public String getCart(@PathVariable String cartId) {
+    return ApiResponse.success(this.cartModel.getCartById(cartId));
+  }
+}
