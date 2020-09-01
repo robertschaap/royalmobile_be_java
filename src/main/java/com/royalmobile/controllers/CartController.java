a2b4f9d5fb8fe191a51328720b1403c4f167e6da
robertschaap <robert.schaap@mac.com>
2020-09-01 17:09:00 +0200

    feat(CartController): add addCartItem base method

--- src/main/java/com/royalmobile/controllers/CartController.java
+++ src/main/java/com/royalmobile/controllers/CartController.java
@@ -3,9 +3,16 @@ package com.royalmobile.controllers;
 import com.royalmobile.ApiResponse;
 import com.royalmobile.models.CartModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * getCart: IF cart in model get cart ELSE create new cart OR create new if 'new'
+ * patchCartItem: add item to cart and return updated cart (CHECK if HTTP method expects this)
+ * deleteCartItem delete item from cart and return updated cart (CHECK if HTTP method expects this)
+ * postOrder: post order
+ */
 @RestController
 public class CartController {
 
@@ -16,6 +23,15 @@ public class CartController {
 
     try {
       return ApiResponse.success(this.cartModel.getCartById(cartId));
+    } catch (Exception e) {
+      return ApiResponse.error("Not Found");
+    }
+  }
+
+  @PatchMapping(value = "/api/cart/{cartId}/item")
+  public String addCartItem(@PathVariable String cartId) {
+    try {
+      return ApiResponse.success(cartModel.addCartItem(cartId));
     } catch (Exception e) {
       return ApiResponse.error("Not Found");
     }
