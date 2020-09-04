3224d93fe671f00143ee1141f822522b8834ff6e
robertschaap <robert.schaap@mac.com>
2020-09-04 20:55:43 +0200

    feat(Connection): setup basic database connection with subscriptions

---
+++ src/main/java/com/royalmobile/db/Connection.java
@@ -0,0 +1,20 @@
+package com.royalmobile.db;
+
+import com.royalmobile.models.Subscription;
+
+public class Connection {
+  private static Subscription[] subscriptions = {
+    new Subscription(0, "20gb", "1year", "20"),
+    new Subscription(1, "40gb", "1year", "30"),
+    new Subscription(2, "60gb", "1year", "40"),
+    new Subscription(3, "80gb", "1year", "50"),
+    new Subscription(4, "20gb", "2year", "18"),
+    new Subscription(5, "40gb", "2year", "28"),
+    new Subscription(6, "60gb", "2year", "38"),
+    new Subscription(7, "80gb", "2year", "48"),
+  };
+
+  public static Subscription[] subscriptions () {
+    return subscriptions;
+  }
+}
