c1cbc2f235b840627a8c03c615bd43f91672c985
robertschaap <robert.schaap@mac.com>
2020-09-05 10:47:58 +0200

    feat(Connection): add carts table

--- src/main/java/com/royalmobile/db/Connection.java
+++ src/main/java/com/royalmobile/db/Connection.java
@@ -3,4 +3,5 @@ package com.royalmobile.db;
 public class Connection {
   public static SubscriptionsTable subscriptions = new SubscriptionsTable();
   public static ProductsTable products = new ProductsTable();
+  public static CartsTable carts = new CartsTable();
 }
