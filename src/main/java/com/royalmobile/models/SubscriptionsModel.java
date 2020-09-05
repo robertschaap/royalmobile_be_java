fd7df5d02525c533b20b6a59ebc2899c7d47607b
robertschaap <robert.schaap@mac.com>
2020-09-05 10:04:07 +0200

    feat(SubscriptionsModel): add database methods

--- src/main/java/com/royalmobile/models/SubscriptionsModel.java
+++ src/main/java/com/royalmobile/models/SubscriptionsModel.java
@@ -4,19 +4,11 @@ import com.royalmobile.db.Connection;
 
 public class SubscriptionsModel {
 
-  private Subscription[] subscriptions = Connection.subscriptions();
-
   public Subscription[] getSubscriptions() {
-    return this.subscriptions;
+    return Connection.subscriptions.getSubscriptions();
   }
 
   public Subscription getSubscriptionById(String subscriptionId) throws Exception {
-    for (Subscription subscription : this.subscriptions) {
-      if (subscription.getSubscriptionId().equals(subscriptionId)) {
-        return subscription;
-      }
-    }
-
-    throw new Exception("Not Found");
+    return Connection.subscriptions.getSubScriptionById(subscriptionId).get();
   }
 }
