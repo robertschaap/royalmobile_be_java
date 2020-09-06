4069a6a0d18576d1ab01721641c69ea9bc0d384f
robertschaap <robert.schaap@mac.com>
2020-09-06 08:16:54 +0200

    feat(MainController): add todo

--- src/main/java/com/royalmobile/controllers/MainController.java
+++ src/main/java/com/royalmobile/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+  // TODO: redirect this or other routes to some sort of fallback
   @GetMapping(value = "/", produces = { "application/json" })
   public String getIndex() {
     return ApiResponse.success("Hello World");
