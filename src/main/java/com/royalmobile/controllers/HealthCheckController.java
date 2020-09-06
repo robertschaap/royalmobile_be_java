da9f27e81b46f5737b4672ed20fd33855025408f
robertschaap <robert.schaap@mac.com>
2020-09-06 08:16:36 +0200

    feat(HealthCheckController): add comments to methods

--- src/main/java/com/royalmobile/controllers/HealthCheckController.java
+++ src/main/java/com/royalmobile/controllers/HealthCheckController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+  /**
+   * @return ApiResponse if the application is running
+   */
   @GetMapping(value = "/healthcheck", produces = { "application/json" })
   public String getHealthCheck() {
     return ApiResponse.success("Application is up and running");
