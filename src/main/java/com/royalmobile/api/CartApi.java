3506365ae9752096b3809016bb4e80c7fcaf7f6f
robertschaap <robert.schaap@mac.com>
2020-09-08 21:07:43 +0200

    feat(CartApi): add postOrder method

--- src/main/java/com/royalmobile/api/CartApi.java
+++ src/main/java/com/royalmobile/api/CartApi.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * getCart: IF cart in model get cart ELSE create new cart OR create new if
- * 'new' patchCartItem: add item to cart and return updated cart (CHECK if HTTP
- * method expects this) deleteCartItem delete item from cart and return updated
- * cart (CHECK if HTTP method expects this) postOrder: post order
- */
 public interface CartApi {
   @GetMapping(value = "/api/cart/{cartId}", produces = { "application/json" })
   public String getCart(@PathVariable String cartId);
@@ -23,4 +18,7 @@ public interface CartApi {
 
   @DeleteMapping(value = "/api/cart/{cartId}/item/{itemId}", produces = { "application/json" })
   public String deleteCartItem(@PathVariable String cartId, @PathVariable String itemId);
+
+  @PostMapping(value = "api/cart/order", produces = { "application/json "})
+  public String postOrder(@RequestParam Map<String, String> body);
 }
