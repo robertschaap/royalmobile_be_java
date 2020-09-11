807e6d6ac989b6fce9b46a8133ea11b1d0721101
robertschaap <robert.schaap@mac.com>
2020-09-11 17:15:58 +0200

    feat(MainController): remove controller

--- src/main/java/com/royalmobile/api/MainApi.java
+++
@@ -1,8 +0,0 @@
-package com.royalmobile.api;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-public interface MainApi {
-  @GetMapping(value = "/", produces = { "application/json" })
-  String getIndex();
-}
