5266dff28fd6233813ab61175631715425ef8272
robertschaap <robert.schaap@mac.com>
2020-09-12 09:53:51 +0200

    refactor(Subscription): move logic to SubscriptionFactory

--- src/main/java/com/royalmobile/models/Subscription.java
+++ src/main/java/com/royalmobile/models/Subscription.java
@@ -2,25 +2,19 @@ package com.royalmobile.models;
 
 public class Subscription {
 
-  // Prevents GSON from exposing this, however consider
-  // splitting into a factory and a separate pojo
-  private transient final String BASE_ID = "royalmobile";
+  private final Integer id;
+  private final String subscriptionId;
+  private final String durationId;
+  private final String data;
+  private final String[] benefits_long = { "unlimited calls", "unlimited texts", "unlimited roaming" };
+  private final String benefits_short = "unlimited\n calls, texts, roaming";
+  private final String regular_price;
 
-  private Integer id;
-  private String subscriptionId;
-  private String durationId;
-  private String data;
-  private String[] benefits_long = { "unlimited calls", "unlimited texts", "unlimited roaming" };
-  private String benefits_short = "unlimited\n calls, texts, roaming";
-  private String regular_price;
-
-  // TODO: this works for now but bleeds our fake db logic into the
-  // actual class which should be a POJO, split them somehow
-  public Subscription(Integer id, String data, String durationId, String price) {
+  public Subscription(Integer id, String subscriptionId, String data, String durationId, String price) {
     this.id = id;
     this.data = data;
     this.durationId = durationId;
-    this.subscriptionId = this.createSubscriptionId(data, durationId);
+    this.subscriptionId = subscriptionId;
     this.regular_price = price;
   }
 
@@ -50,9 +44,5 @@ public class Subscription {
 
   public String getRegular_price() {
     return this.regular_price;
-  }
-
-  private String createSubscriptionId(String data, String durationId) {
-    return this.BASE_ID + "-" + data + "-" + durationId;
   }
 }
--- src/main/java/com/royalmobile/models/SubscriptionFactory.java
+++ src/main/java/com/royalmobile/models/SubscriptionFactory.java
@@ -1,7 +1,15 @@
 package com.royalmobile.models;
 
 public class SubscriptionFactory {
+  private final String BASE_ID = "royalmobile";
+
   public Subscription createSubscription(Integer id, String data, String durationId, String price) {
-    return new Subscription(id, data, durationId, price);
+    String subscriptionId = this.createSubscriptionId(data, durationId);
+
+    return new Subscription(id, subscriptionId, data, durationId, price);
+  }
+
+  private String createSubscriptionId(String data, String durationId) {
+    return this.BASE_ID + "-" + data + "-" + durationId;
   }
 }
